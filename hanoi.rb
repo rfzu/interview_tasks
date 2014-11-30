a822d9705e3df7fe4c4090c48f43f32d3b95c467
rfzu <you@example.com>
2014-12-01 02:35:52 +0300

    add hanoi

---
+++ hanoi.rb
@@ -0,0 +1,40 @@
+=begin
+Составить алгоритм для решения задачи о ханойских башнях.
+Для 3-х дисков
+=end
+
+#a,b,c - массивы башни, числа в массивых - кольца, 
+#значение соответсвует размеру кольца
+#n - размер
+
+def hanoi(n, han=nil, from=nil, to=nil)
+  p han ||= [[*1..n].reverse,[],[]]
+  from ||= 0
+  to ||= 2
+  buf = [0,1,2].index {|i| i != to && i != from }
+
+  if n == 1
+    han[to] << han[from].pop
+    p han
+  elsif n==2
+    to, buf = buf, to
+    hanoi(1, han, from, to)
+    to, buf = buf, to
+    han[to] << han[from].pop
+    from, buf = buf, from
+    hanoi(1, han, from, to)
+    p han
+  else
+    to, buf = buf, to
+    hanoi(n-1, han, from, to)
+    to, buf = buf, to
+    han[to] << han[from].pop
+    from,buf = buf, from
+    p han
+    hanoi(n-1, han, from, to)
+    p han
+  end
+  han
+end
+
+hanoi(7)
